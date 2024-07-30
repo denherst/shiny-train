5f972b56b2695a73d1ff539d13d274113e2e75b3
denherst <100481983+denherst@users.noreply.github.com>
2024-07-30 20:30:47 +0300

    Update w.lua

--- w.lua
+++ w.lua
@@ -1,7 +1,7 @@
 repeat wait() until game:IsLoaded()
 Instance.new("Message", workspace).Text = "lol no"
 game.Players.LocalPlayer.OnTeleport:Connect(function(State)
-    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/denherst/shiny-train/main/w.lua?token=GHSAT0AAAAAACUWEHGF7KP5NMZD46AR7SGYZVJEI4A"))()')
+    queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/denherst/shiny-train/main/w.lua"))()')
 end)  
 
 task.wait(10)
