69d8763683d88b840ba448b091116d4fea192501
denherst <100481983+denherst@users.noreply.github.com>
2024-07-30 20:51:38 +0300

    Update w.lua

--- w.lua
+++ w.lua
@@ -1,10 +1,38 @@
 repeat wait() until game:IsLoaded()
 local module = loadstring(game:HttpGet"https://raw.githubusercontent.com/LeoKholYt/roblox/main/lk_serverhop.lua")()
-Instance.new("Message", workspace).Text = "lol no"
+local me = game.Players.LocalPlayer
+local currentpos
+
+pcall(function()
+    function getkey()
+        workspace["locked parts [do not select]"].nukedoor1:Destroy()
+        workspace["locked parts [do not select]"].keyers.ClickDetector.MaxActivationDistance = 100
+        fireclickdetector(workspace["locked parts [do not select]"].keyers.ClickDetector)
+        me.Backpack:WaitForChild("Nuclear Key").Parent = me.Character
+    end
+    
+    currentpos = me.Character.HumanoidRootPart.CFrame
+    task.wait(0.1)
+    me.Character.HumanoidRootPart.CFrame = CFrame.new(-2.16781926, 3.02549911, 112.205048, -0.999847651, 1.13101457e-08, 0.0174543373, 1.12859233e-08, 1, -1.48625978e-09, -0.0174543373, -1.28904509e-09, -0.999847651)
+    task.wait(0.1)
+    getkey()
+    task.wait(0.25)
+    me.Character.HumanoidRootPart.CFrame = CFrame.new(2.22531891, 3.42039919, 115.998581, 0.999795794, 3.63750168e-08, -0.0202076808, -3.46687514e-08, 1, 8.47868336e-08, 0.0202076808, -8.40689438e-08, 0.999795794)
+    task.wait(0.1)
+    getkey()
+    task.wait(0.25)
+    fireclickdetector(workspace["locked parts [do not select]"].Button.Button.ClickDetector)
+    task.wait(0.1)
+    me.Character.HumanoidRootPart.CFrame = currentpos
+end)
+
 game.Players.LocalPlayer.OnTeleport:Connect(function(State)
     queue_on_teleport('loadstring(game:HttpGet("https://raw.githubusercontent.com/denherst/shiny-train/main/w.lua"))()')
 end)  
 
-task.wait(10)
+task.wait(1)
 
-module:Teleport(game.PlaceId)
+game:GetService("TeleportService").TeleportInitFailed:Connect(function(player, teleportResult, errorMessage, placeId, teleportOptions)
+    task.wait(1)
+    module:Teleport(game.PlaceId)
+end)
